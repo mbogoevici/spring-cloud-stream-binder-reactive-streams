d986ef9838a10f7b1a11a3437f55f5ab895cf2e6
Marius Bogoevici <mbogoevici@pivotal.io>
2017-02-21 19:22:35 -0500

    Test tweaks

--- samples/spring-cloud-stream-reactive-binder-samples-rxhttp/src/test/java/devnexus2017/demo/reactivehttp/DemoApplicationTests.java
+++ samples/spring-cloud-stream-reactive-binder-samples-rxhttp/src/test/java/devnexus2017/demo/reactivehttp/DemoApplicationTests.java
@@ -1,12 +1,15 @@
 package devnexus2017.demo.reactivehttp;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Ignore
 public class DemoApplicationTests {
 
 	@Test
